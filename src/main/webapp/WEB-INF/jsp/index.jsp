4e5aa29ff60522291376753a17d42cb3ce1b5cc7
jinheyokp <parkjinhyeok87@gmail.com>
2024-02-23 15:30:31 +0900

    feat: map list in map left side, all db list showing in list, body: next job is limit the marker by size of display,

--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -5,6 +5,7 @@
 <html>
 <head>
     <title>Hello Page</title>
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/resource/css/index.css">
     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=63e52530f05ff3afd5fe6e56e3f067a4&libraries=services,clusterer,drawing"></script>
 </head>
 <body>
@@ -15,7 +16,23 @@
 <form action="/signup">
     <input type="submit" value="signup" />
 </form>
-<div id="map" style="width: 500px; height: 400px;"></div>
+<div class="map_wrap">
+    <div id="map" style="width:100%;height:100%;position:relative;overflow:hidden;"></div>
+
+    <div id="menu_wrap" class="bg_white">
+        <div class="option">
+            <div>
+                <form onsubmit="searchPlaces(); return false;">
+                    키워드 : <input type="text" value="default" id="keyword" size="13">
+                    <button type="submit">검색하기</button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <ul id="placesList"></ul>
+        <div id="pagination"></div>
+    </div>
+</div>
 <script>
  var postData = JSON.parse('${post}');
 </script>
--- src/main/webapp/resource/js/index.js
+++ src/main/webapp/resource/js/index.js
@@ -1,44 +1,103 @@
 const script = document.createElement("script");
 script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=63e52530f05ff3afd5fe6e56e3f067a4&libraries=services,clusterer,drawing";
 document.head.appendChild(script);
+
 script.onload = () => {
-kakao.maps.load(() => {
-    const node = document.getElementById('map');
-    const center = new kakao.maps.LatLng(37.50802, 127.062835);
-    const options =
-    {
-        center,
-        level: 3
-    };
+    kakao.maps.load(() => {
+        const node = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(37.50802, 127.062835),
+            level: 3
+        };
 
-  const map = new kakao.maps.Map(node, options); // 지도 생성
-  var markers = []; // 마커를 저장할 배열
+        const map = new kakao.maps.Map(node, options); // 지도 생성
+        var markers = []; // 마커를 저장할 배열
 
-  postData.forEach(function (ele) {
-      var markerPosition = new kakao.maps.LatLng(ele.axis_x, ele.axis_y); // 마커 위치 설정
-      var marker = new kakao.maps.Marker({ position: markerPosition }); // 마커 생성
-      marker.setMap(map); // 마커를 지도에 표시
-      markers.push(marker); // 마커 배열에 추가
+        var listEl = document.getElementById('placesList'),
+            fragment = document.createDocumentFragment(),
+            bounds = new kakao.maps.LatLngBounds();
 
-      var infowindow = new kakao.maps.InfoWindow({
-          content: `<div style="padding:5px;">${ele.title}</div>`, // 인포윈도우에 표시될 내용
-          removable: true // 인포윈도우를 닫을 수 있는 x버튼 표시 여부
-      });
-      infowindow.open(map, marker); // 인포윈도우 표시
+        removeAllChildNodes(listEl);
+        removeMarker();
 
-      // 마커에 클릭 이벤트를 등록합니다.
-      kakao.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map, marker); // 마커 클릭 시 인포윈도우를 표시합니다.
-      });
-  });
+        postData.forEach(function(ele) {
+            var markerPosition = new kakao.maps.LatLng(ele.axis_x, ele.axis_y); // 마커 위치 설정
+            var marker = addMarker(markerPosition, ele.proposal_id - 1, ele.title);
+            var itemEl = getListItem(ele.proposal_id, ele); // 수정된 getListItem 함수를 사용
+            bounds.extend(markerPosition);
+            fragment.appendChild(itemEl);
 
-  // 지도에 컨트롤 추가
-  var mapTypeControl = new kakao.maps.MapTypeControl();
-  map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT); // 지도 타입 변경 컨트롤을 지도에 추가
+            markers.push(marker); // 마커 배열에 추가
+        });
 
-  var zoomControl = new kakao.maps.ZoomControl();
-  map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT); // 줌 컨트롤을 지도에 추가
+        listEl.appendChild(fragment);
+        map.setBounds(bounds);
 
+        // 지도에 컨트롤 추가
+        var mapTypeControl = new kakao.maps.MapTypeControl();
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
+        var zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+        function addMarker(position, idx, title) {
+            var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png',
+                imageSize = new kakao.maps.Size(36, 37),
+                imgOptions = {
+                    spriteSize: new kakao.maps.Size(36, 691),
+                    spriteOrigin: new kakao.maps.Point(0, (idx * 46) + 10),
+                    offset: new kakao.maps.Point(13, 37)
+                },
+                markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
+                marker = new kakao.maps.Marker({
+                    position: position,
+                    image: markerImage
+                });
+
+            var infowindow = new kakao.maps.InfoWindow({
+                content: `<div style="padding:5px;">${title}</div>`
+            });
+
+            kakao.maps.event.addListener(marker, 'mouseover', function() {
+                infowindow.open(map, marker);
+            });
+            kakao.maps.event.addListener(marker, 'mouseout', function() {
+                infowindow.close();
+            });
+
+            marker.setMap(map);
+            return marker;
+        }
+
+        function removeAllChildNodes(el) {
+            while (el.hasChildNodes()) {
+                el.removeChild(el.lastChild);
+            }
+        }
+
+        function removeMarker() {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+            markers = [];
+        }
+
+        function getListItem(index, data) {
+            var el = document.createElement('li'),
+                itemStr = `<span class="markerbg marker_${index}"></span>
+                            <div class="info">
+                                <h5>${data.title}</h5>
+                                <span>${data.address}</span>`;
+
+            if (data.tel) {
+                itemStr += `<span class="tel">${data.tel}</span>`;
+            }
+
+            itemStr += '</div>';
+            el.innerHTML = itemStr;
+            el.className = 'item';
+
+            return el;
+        }
     });
 };
